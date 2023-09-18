cd72540290821a25d8e97860a006fe2b8f52625d
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2023-09-18 14:04:01 -0600

    Updated arraydoubling folder

--- arraydoubling/arraydoubling.cpp
+++ arraydoubling/arraydoubling.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+//return a pointer example
+
+
+//pass a pointer by refernece example
+void arr2xRef(int*&a, int&n){
+	int *temp = new int[2*n];
+	for(int i = 0; i < n; i++}{
+		temp[i]	= a[i];	
+	}
+	delete [] a;
+	a = temp;
+	n = 2*n;
+}
+
 int main()
 {
-	
+	//pass a pointer by ???
+	//pass a pointer by reference example
+	//	
 	return 0;	
 }
