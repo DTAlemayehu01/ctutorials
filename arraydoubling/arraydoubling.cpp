c376dc7e73b18511980b59caad933c77ff7fe17a
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2023-09-18 14:11:17 -0600

    Updated arraydoubling dir

--- arraydoubling/arraydoubling.cpp
+++ arraydoubling/arraydoubling.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+//dont be stingy with variables, compilers will optimize out the variables to keep runtime as fast as possible
 using namespace std;
 
 //return a pointer example
@@ -16,10 +16,30 @@ void arr2xRef(int*&a, int&n){
 	n = 2*n;
 }
 
+//Pass a pointer-pointer variable example (possible in C)
+void arr2xPP(int **a, int *n){
+	int *temp = new int[2*(*n)];
+	for(int i = 0; i<*n; i++){
+		temp[i] = a[i];
+	}
+	delete[]*a;
+	*a = temp;
+	*n = 2*(*n);
+}
+
 int main()
 {
 	//pass a pointer by ???
+	
+	
 	//pass a pointer by reference example
-	//	
+	
+	
+	//pass a pointer-pointer variable example
+	int length3 = 2;
+	int *array = new int[2];
+	int **arr_pt = &array;
+	arr2xPP(arr_pt, &length3);
+
 	return 0;	
 }
