1533ebafaf0027603db22d7c89f3f56246d5057e
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2023-11-13 13:37:45 -0700

    Added variables.cpp

---
+++ OrgRebase/variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main () {
+    int num = 20;
+    char favChar = 'A';
+    string name = "John";
+
+    std::cout << "There once was a man named " << name << "." << std::endl;
+    std::cout << "He was " << num << " years old." << std::endl;
+    std::cout << "His favorite letter is " << favChar << std::endl;
+    std::cout << "He liked his name " << name << " but he wanted to be older than " << age << "." << std:: endl;
+}
