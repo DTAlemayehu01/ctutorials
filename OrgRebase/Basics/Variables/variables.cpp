df368edc6b76da83e4a2af119bcec8a63a19e4b2
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2023-11-20 18:02:42 -0700

    organized files, automated code evaluation

--- OrgRebase/Basics/Variables/variables.cpp
+++ OrgRebase/Basics/Variables/variables.cpp
@@ -3,10 +3,10 @@
 int main () {
     int num = 20;
     char favChar = 'A';
-    string name = "John";
+    std::string name = "John";
 
     std::cout << "There once was a man named " << name << "." << std::endl;
     std::cout << "He was " << num << " years old." << std::endl;
     std::cout << "His favorite letter is " << favChar << std::endl;
-    std::cout << "He liked his name " << name << " but he wanted to be older than " << age << "." << std:: endl;
+    std::cout << "He liked his name " << name << " but he wanted to be older than " << num << "." << std:: endl;
 }
