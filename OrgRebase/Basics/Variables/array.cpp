339d1e625966eb9d55833bad5c8bbec537998bf1
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2023-11-20 19:47:06 -0700

    Removed obsolete files, expaneded variables.org

---
+++ OrgRebase/Basics/Variables/array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main () {
+    int arrayOfNums[] = {4, 0, 8, 12};
+    std::cout << arrayOfNums[0] << std::endl;
+}
+
+#include<iostream>
+int main() {
+    int matrix[2][2] = {
+                        {2,4},
+                        {6,8}
+                        };
+    std::cout << matrix[1][0] << std::endl;
+}
