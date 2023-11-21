90ada84993323953876417c97af4691dae9ce1f4
DTA1 <dirgasands5673@proton.me>
2023-11-21 14:32:12 -0700

    Renamed file to represent changes to it

---
+++ cmath.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+//pow(x, y) does x^y
+//sqrt(x,y) does x^1/y
+//round(x) rounds x to nearest int
+//ceil(x) rounds up to closest int
+//fmax(x,y) returns bigger number fmin() does opposite
+
+int main()
+{
+	cout << 5 << endl;
+	return 0;
+}
