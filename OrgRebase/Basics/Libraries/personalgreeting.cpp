1cde212f00ddd15fd7583d828880513e7ff773c6
DTA1 <dirgasands5673@proton.me>
2023-11-23 16:14:22 -0700

    Tangled libraries.org

---
+++ OrgRebase/Basics/Libraries/personalgreeting.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main() {
+    std::string string;
+    std::cout << "What is your name? ";
+    std::cin >> string;
+    std::cout << "What an interesting name, " << string << std::endl;
+}
