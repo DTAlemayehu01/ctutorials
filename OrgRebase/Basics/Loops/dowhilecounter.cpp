2e6345b8c638a1072898cc915c58bf90e088df29
DTA1 <dirgasands5673@proton.me>
2023-11-23 03:59:22 -0700

    Started libraries and functions .org files

--- OrgRebase/Basics/Loops/dowhilecounter.cpp
+++ OrgRebase/Basics/Loops/dowhilecounter.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 int main () {
-    int number = 1;
-    while(number < 6) {
+    //make sure to assign numbers to these variables.
+    int number;
+    do {
         std::cout << number << " mississippi" << std::endl;
         number++;
-    }
+    } while(number < 6);
 }
